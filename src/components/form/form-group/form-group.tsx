b1cb1359da2a2d353ea45ac228f5c8932e6614e5
a.karpenko <oksud22@gmail.com>
2025-04-20 20:07:48 +0300

    add separate field for required flag

--- src/components/form/form-group/form-group.tsx
+++ src/components/form/form-group/form-group.tsx
@@ -9,15 +9,17 @@ import { FormControlLabel } from '../form-control-label';
 interface FormFieldProps {
   label?: string;
   formFieldName: string;
+  isRequired?: boolean;
 }
 
 export const FormGroup: ExtendedReactFunctionalComponent<FormFieldProps> = ({
   label,
   formFieldName,
+  isRequired,
   children,
 }) => {
   return (
-    <FormikFormControl formFieldName={formFieldName}>
+    <FormikFormControl isRequired={isRequired} formFieldName={formFieldName}>
       <Show.When isTrue={Boolean(label)}>
         <FormControlLabel>{label}</FormControlLabel>
       </Show.When>
