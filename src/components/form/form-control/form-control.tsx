0e09d7298510b34cf058e5062e6f3549d9622480
a.karpenko <oksud22@gmail.com>
2025-04-21 16:24:49 +0300

    fixed layout directions

--- src/components/form/form-control/form-control.tsx
+++ src/components/form/form-control/form-control.tsx
@@ -1,11 +1,11 @@
 
 import React from 'react';
 
-import { ClusterLayout } from "src/components/layout";
+import { StackLayout } from "src/components/layout";
 import { ExtendedReactFunctionalComponent } from "src/types";
 
 export const FormControl: ExtendedReactFunctionalComponent = ({ children, ...rest }) => {
   return (
-    <ClusterLayout space="0.5rem" {...rest}>{children}</ClusterLayout>
+    <StackLayout space="0.5rem" {...rest}>{children}</StackLayout>
   )
 }
