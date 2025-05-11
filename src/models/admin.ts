5fc9e781d28941b93861cadfaef32add06421a78
a.karpenko <oksud22@gmail.com>
2025-05-11 15:27:52 +0300

    update admin model

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -1,14 +1,17 @@
+import { UserType } from "src/enums";
+import { File } from "./file";
+
 export interface Admin {
   _id: string;
   username: string;
   surname: string;
   password: string;
-  avatarUrl: string;
-  isPublic: boolean;
-  address: string;
+  avatar: File;
   city: string;
-  privateToken: string;
   country: string;
+  privateKey: string;
+  privateToken: string;
   email: string;
   primaryPhone: string;
+  type: UserType;
 }
