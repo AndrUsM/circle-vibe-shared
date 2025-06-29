cd34dfb9810398c5b2bb9428b88b1bd74a5eef24
a.karpenko <oksud22@gmail.com>
2025-06-29 22:17:15 +0300

    fix 2

--- src/components/form/form-control-textarea/form-control-textarea.tsx
+++ src/components/form/form-control-textarea/form-control-textarea.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import TextareaAutosize from 'react-textarea-autosize';
 import { CustomCssVariables } from '../../../types/custom-css-variables';
 import { ExtendedReactFunctionalComponent } from '../../../types/extended-react-functional-component';
 import { FieldContext } from '../field-context/field-context';
@@ -15,39 +14,45 @@ export const FormControlTextarea: ExtendedReactFunctionalComponent<
   const fieldProvider = useContext(FieldContext);
 
   if (typeof window !== 'undefined') {
-    <TextareaAutosize
-      {...props}
-      {...fieldProvider}
-      inputMode={props.inputMode ?? 'text'}
-      onChange={(e) => {
-        const value = getFormikTextreaFormControlValue(e);
-        fieldProvider.setValue(value as unknown as string);
-      }}
-      className="text-input-generic-styles"
-      // @ts-ignore
-      style={
-        {
-          '--text-input-border-color':
-            (fieldProvider.errors ?? '')?.length > 0
-              ? 'var(--text-input-error-border-color)'
-              : 'var(--text-input-default-border-color)',
-          '--text-input-text-color': `var(${
-            (fieldProvider.errors ?? '')?.length > 0
-              ? '--text-input-error-text-color'
-              : '--text-input-default-text-color'
-          })`,
-          '--text-input-outline-color':
-            (fieldProvider.errors ?? '')?.length > 0
-              ? 'var(--text-input-error-outline-color)'
-              : 'transparent',
-          '--text-input-background': `var(${
-            (fieldProvider.errors ?? '')?.length > 0
-              ? '--text-input-error-bg-color'
-              : '--text-input-default-bg-color'
-          })`,
-        } as CustomCssVariables
-      }
-    />;
+    import('react-textarea-autosize').then((TextareaAutosize) => {
+      return (
+        // @ts-ignore
+        <TextareaAutosize
+          {...props}
+          {...fieldProvider}
+          inputMode={props.inputMode ?? 'text'}
+          // @ts-ignore
+          onChange={(e) => {
+            const value = getFormikTextreaFormControlValue(e);
+            fieldProvider.setValue(value as unknown as string);
+          }}
+          className="text-input-generic-styles"
+          // @ts-ignore
+          style={
+            {
+              '--text-input-border-color':
+                (fieldProvider.errors ?? '')?.length > 0
+                  ? 'var(--text-input-error-border-color)'
+                  : 'var(--text-input-default-border-color)',
+              '--text-input-text-color': `var(${
+                (fieldProvider.errors ?? '')?.length > 0
+                  ? '--text-input-error-text-color'
+                  : '--text-input-default-text-color'
+              })`,
+              '--text-input-outline-color':
+                (fieldProvider.errors ?? '')?.length > 0
+                  ? 'var(--text-input-error-outline-color)'
+                  : 'transparent',
+              '--text-input-background': `var(${
+                (fieldProvider.errors ?? '')?.length > 0
+                  ? '--text-input-error-bg-color'
+                  : '--text-input-default-bg-color'
+              })`,
+            } as CustomCssVariables
+          }
+        />
+      );
+    });
   }
 
   return <></>;
