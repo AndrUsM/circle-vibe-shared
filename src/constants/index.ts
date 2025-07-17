c202a693e7e6e2941f57cca0166d368d6b88cf46
a.karpenko <oksud22@gmail.com>
2025-07-17 23:50:37 +0300

    removed unused dependecies added security-service-config

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,4 @@ export * from './nationality-codes';
 export * from './ms-document.regex';
 export * from './document-regex';
 export * from './default-pagination-page-size';
+export * from './security-service-config';
---
+++ src/constants/security-service-config.ts
@@ -0,0 +1,7 @@
+export const ALGORITHM = 'aes-256-gcm';
+export const IV_LENGTH = 12;
+export const SALT_LENGTH = 16;
+export const KEY_LENGTH = 32;
+export const TAG_LENGTH = 16;
+export const PBKDF2_ITERATIONS = 100000;
+export const DIGEST = 'sha512';
