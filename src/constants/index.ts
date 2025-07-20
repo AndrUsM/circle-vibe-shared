2f5cc8e369207c260f607d6b79be53f0f50a7a09
a.karpenko <oksud22@gmail.com>
2025-07-20 18:34:17 +0300

    removed secrurity utils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,4 +5,3 @@ export * from './nationality-codes';
 export * from './ms-document.regex';
 export * from './document-regex';
 export * from './default-pagination-page-size';
-export * from './security-service-config';
